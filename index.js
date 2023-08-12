955f89057fa0d53e5aa8b6ccdc0f10a0bc74d925
Chetanp203 <patil.chetan1994@gmail.com>
2023-08-12 11:43:29 +0530

    update

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 import express from "express";
 import { login, register } from "./Controllers/User.controllers.js";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 
 app.get("/",function(req,res){
  res.send("welcome to backend")
 
 })
-app.get("/login",login)
+// app.get("/login",login)
 
-app.get("/register",register)
+// app.get("/register",register)
+
+app.post('/login',function(req,res){
+  res.send("Hello from login function")
+})
+
+app.post('/register',function(req,res){
+  res.send("Register here")
+})
 
 mongoose.connect("mongodb+srv://chetan203:chetan203@cluster0.qocfrn6.mongodb.net/REACT-BACKEND").then(()=>{
     console.log("connected to mongoDB")
