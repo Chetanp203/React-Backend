4120b25ccb71cff22eae8389f6815af23c848299
Chetanp203 <patil.chetan1994@gmail.com>
2023-08-17 23:33:20 +0530

    update

--- 13-08-23/Controllers/Product-controller.js
+++ 13-08-23/Controllers/Product-controller.js
@@ -7,9 +7,18 @@ export const addProduct = async (req,res)=>{
     try{
      const {name, price, image, category,token} = req.body;
      if(!name || !price || !image || !category || !token) return res.status(404).json({status:"error",message:"All fields are mandatory"})
+    
+     const decodedData = jwt.verify(token, process.env.JWT_SECRET)
 
+     if(!decodedData){
+         return res.status(404).json({status:"error",message:"Token not valid"});
+     }
+  
+      const userId = decodedData.userID;
+ 
+      
 
-    const product = new ProductModal({name, price, image, category});
+    const product = new ProductModal({name, price, image, category,userId:userId});
 
     await product.save();
 
@@ -17,5 +26,68 @@ export const addProduct = async (req,res)=>{
 
     }catch(error){
         return res.status(500).json({status:"error", error: error.message})
+    }
+}
+
+
+export const allProducts =async (req,res)=>{
+    try{
+        const products = await ProductModal.find({});
+        if(products.length){
+            return res.status(200).json({status:"success",products: products})
+        }
+        return res.status(404).json({status:"error",message:"No products found"})
+
+    }catch(error){
+        return res.status(500).json({status:"error", error:error.message})
+    }
+}
+
+
+export const getYourProducts= async (req,res)=>{
+    try{
+        const {token}= req.body;
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET)
+
+     if(!decodedData){
+         return res.status(404).json({status:"error",message:"Token not valid"});
+     }
+  
+      const userId = decodedData.userID;
+        
+      const yourProductData = await ProductModal.find({userId:userId})
+
+      if(yourProductData.length){
+        return res.status(200).json({status:"success",products: yourProductData})
+      }
+      return res.status(404).json({status:"error",message:"No products found.."})
+
+    }catch(error){
+        return res.status(500).json({status:"error",error:error.message})
+    }
+}
+
+export const updateYourProduct = async (req,res)=>{
+    try{
+    const {productId,name,image,price,category,token}= req.body;
+    if(!token) return res.status(404).json({status:"error",message:"Token is mandatory"})
+
+    const decodedData = jwt.verify(token, process.env.JWT_SECRET)
+
+    if(!decodedData){
+        return res.status(404).json({status:"error",message:"Token not valid"});
+    }
+ 
+     const userId = decodedData.userID;
+   
+    const updatedProduct = await ProductModal.findOneAndUpdate({_id: productId,userId:userId},{name,image,price,category},{new: true})
+
+    if(updatedProduct){
+        return res.status(200).json({staus:"Sucess",product: updatedProduct})
+    }
+    return res.status(404).json({status:"error",message:"Access denied unauthorised user "})
+
+    }catch(error){
+        return res.status(500).json({status:"error", error:error.message})
     }
 }
--- 13-08-23/index.js
+++ 13-08-23/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { getCurrentUser, login, register } from "./Controllers/User-controller.js";
-import { addProduct } from "./Controllers/Product-controller.js";
+import { addProduct, allProducts, getYourProducts, updateYourProduct } from "./Controllers/Product-controller.js";
 import { checkSeller } from "./Middlewares/Seller.Middleware.js";
 
 
@@ -21,6 +21,12 @@ app.post("/login",login)
 app.post("/get-current-user",getCurrentUser)
 
 app.post("/add-product",checkSeller, addProduct)
+
+app.get("/all-products", allProducts)
+
+app.get("/get-your-products",checkSeller, getYourProducts)
+
+app.patch("/update-your-product",checkSeller, updateYourProduct)
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Connected to MongoDB")
